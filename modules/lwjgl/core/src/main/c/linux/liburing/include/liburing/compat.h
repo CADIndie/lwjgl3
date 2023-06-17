c1822ffb9135e92be7c3fe91a10bb0df17feec80
khanhduytran0 <khanhduytran0@users.noreply.github.com>
2023-06-17 19:13:00 +0700

    Start porting to Android and iOS

--- modules/lwjgl/core/src/main/c/linux/liburing/include/liburing/compat.h
+++ modules/lwjgl/core/src/main/c/linux/liburing/include/liburing/compat.h
@@ -10,10 +10,12 @@
 
 typedef int __kernel_rwf_t;
 
+#ifndef __ANDROID__
 struct __kernel_timespec {
 	int64_t		tv_sec;
 	long long	tv_nsec;
 };
+#endif
 
 /* <linux/time_types.h> is not available, so it can't be included */
 #define UAPI_LINUX_IO_URING_H_SKIP_LINUX_TIME_TYPES_H 1
