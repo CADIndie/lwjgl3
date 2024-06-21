fce8090381b3512683b2c63f54dc100fe0fafbaf
Mathias-Boulay <mathiasboulay@free.fr>
2024-06-21 19:52:45 +0200

    fix(build): fix build for 3.3.3
    We still need to ship freetype tough

--- modules/lwjgl/core/src/main/c/linux/liburing/include/liburing/compat.h
+++ modules/lwjgl/core/src/main/c/linux/liburing/include/liburing/compat.h
@@ -15,7 +15,6 @@ struct __kernel_timespec {
 	int64_t		tv_sec;
 	long long	tv_nsec;
 };
-#endif
 
 /* <linux/time_types.h> is not available, so it can't be included */
 #define UAPI_LINUX_IO_URING_H_SKIP_LINUX_TIME_TYPES_H 1
@@ -25,6 +24,7 @@ struct open_how {
 	uint64_t	mode;
 	uint64_t	resolve;
 };
+#endif
 
 #ifndef BLOCK_URING_CMD_DISCARD
 #define BLOCK_URING_CMD_DISCARD                        _IO(0x12, 0)
