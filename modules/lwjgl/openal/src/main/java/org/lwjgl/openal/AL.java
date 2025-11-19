31beb947d640d762787bf467c0e5ea35e1a386d8
alexytomi <60690056+alexytomi@users.noreply.github.com>
2025-11-19 21:41:40 +0800

    Fix: Use the correct handle when making openal context
    Fixes PolyPatcher's sound thing

--- modules/lwjgl/openal/src/main/java/org/lwjgl/openal/AL.java
+++ modules/lwjgl/openal/src/main/java/org/lwjgl/openal/AL.java
@@ -54,7 +54,7 @@ public final class AL {
     static ALCdevice alcDevice;
     static ALCCapabilities alContextCaps;
     static ALCapabilities alCaps;
-    
+
     private static boolean created_lwjgl2 = false;
 
     /**
@@ -99,10 +99,9 @@ public final class AL {
             attribs.put(0);
             attribs.flip();
 
-            long contextHandle = ALC10.alcCreateContext(alDevice, attribs);
-            ALC10.alcMakeContextCurrent(contextHandle);
             //alContext = new ALContext(alDevice, contextHandle);
-            alContext = ALC10.alcCreateContext(contextHandle, (IntBuffer)null);
+            alContext = ALC10.alcCreateContext(alDevice, attribs);
+            ALC10.alcMakeContextCurrent(alContext);
             alContextCaps = ALC.createCapabilities(alDevice);
 
             alCaps = AL.createCapabilities(alContextCaps);
@@ -134,10 +133,9 @@ public final class AL {
             attribs.put(0);
             attribs.flip();
 
-            long contextHandle = ALC10.alcCreateContext(alDevice, attribs);
-            ALC10.alcMakeContextCurrent(contextHandle);
             //alContext = new ALContext(alDevice, contextHandle);
-            alContext = ALC10.alcCreateContext(contextHandle, (IntBuffer)null);
+            alContext = ALC10.alcCreateContext(alDevice, attribs);
+            ALC10.alcMakeContextCurrent(alContext);
             alContextCaps = ALC.createCapabilities(alDevice);
 
             alCaps = AL.createCapabilities(alContextCaps);
@@ -173,7 +171,7 @@ public final class AL {
             ALC10.alcMakeContextCurrent(MemoryUtil.NULL);
             ALC10.alcDestroyContext(alContext);
             ALC10.alcCloseDevice(alcDevice.device);
-            alContext = -1;
+            alContext = MemoryUtil.NULL;
             alcDevice = null;
             created_lwjgl2 = false;
         }
